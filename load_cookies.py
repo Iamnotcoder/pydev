2ee89fecd0fb8cad33f2f5d1d5d8c3af72d93fbf
Iamnotcoder <313688893@qq.com>
2017-05-28 21:45:58 +0800

    add read network cookies module

---
+++ load_cookies.py
@@ -0,0 +1,15 @@
+#coding=utf8
+# wei.wang what hurts more,the pain of hard work or the pain of regret?
+__author__ = 'v'
+__date__ = '2017/5/28'
+
+import requests
+
+
+def load_cookies():
+    with open('raw_cookies.txt') as f:
+        cookies = {}
+        for line in f.read().split(';'):
+            key, val = line.strip().split('=', 1)
+            cookies[key] = val
+print(cookies)
